3636b43351fb9beffd2403a7ecb4c3733e33f853
reyarias <raarias@fullsail.edu>
2015-03-30 16:58:16 -0700

    prompted user for name
    prompted user for name

--- SetupGitHub/js/script.js
+++ SetupGitHub/js/script.js
@@ -5,3 +5,7 @@ Date :) (Just copied form the video)
 
 alert("Testing 1, 2, 3");
 */
+console.log("Testing the console.log");
+
+var userInput = prompt("Typer in your name:");
+console.log(userInput);
