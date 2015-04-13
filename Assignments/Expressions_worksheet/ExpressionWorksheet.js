278050e8a4d6ddde2461fb5c2a2f571c7e3d7891
reyarias <raarias@fullsail.edu>
2015-04-13 15:30:26 -0700

    Solved The Dog Age Problem
    Solved The Dog Age Problem.

--- Assignments/Expressions_worksheet/ExpressionWorksheet.js
+++ Assignments/Expressions_worksheet/ExpressionWorksheet.js
@@ -2,3 +2,7 @@
  * Created by reyarias1 on 4/13/15.
  */
 //Rey Arias, 04/11/15
+    //Dog Age In Human Years
+var SparkyAge = 11; //How long the dog has been alive in years
+var DogHumanAge = SparkyAge * 7; //the age of the dog in human years
+console.log("Sparky is", SparkyAge, "human years old which is", DogHumanAge, "in dog years")
